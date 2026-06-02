c84983b66758075f847777503dce4d0d69714262
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-02 20:44:50 +0300

    Migrate Db to postgresql

--- Prisma.Infrastructure/DependenciesInjection.cs
+++ Prisma.Infrastructure/DependenciesInjection.cs
@@ -12,7 +12,21 @@ public static class DependenciesInjection
     public static void AddInfrastructureServices(IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultSqlConnection")));
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultSqlConnection"), npgsqlOptions =>
+            {
+                npgsqlOptions.EnableRetryOnFailure(
+                    maxRetryCount: 5,
+                    maxRetryDelay: TimeSpan.FromSeconds(10),
+                    errorCodesToAdd: null);
+            });
+
+            if (environment.IsDevelopment())
+            {
+                options.EnableSensitiveDataLogging();
+                options.EnableDetailedErrors();
+            }
+        });
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
         services.AddScoped(typeof(IRepository<>), typeof(Repository<>));

2412a8aafe370c7ab4c7f3acd17dfbf5c2e8ab8e
moaz-37 <moaz.sameh.37@gmail.com>
2026-06-03 18:41:40 +0300

    Added Audit Entity

---
+++ Prisma.Domain/Entities/Audit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prisma.Domain.Entities;
+
+public class Audit
+{
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt{ get; set; }
+    public DateTime? DeletedAt{ get; set; }
+    public string? CreatedBy{ get; set; }
+    public string? ModifiedBy{ get; set; }
+    public string? DeletedBy{ get; set; }
+    public bool IsDeleted{ get; set; }
+
+}

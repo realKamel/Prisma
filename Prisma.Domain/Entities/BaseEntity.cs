b8ff3f7204cde24bb853a33a591b93b377f966a5
moaz-37 <moaz.sameh.37@gmail.com>
2026-06-03 19:33:17 +0300

    Added BaseEntity

--- Prisma.Domain/Entities/BaseEntity.cs
+++ Prisma.Domain/Entities/BaseEntity.cs
@@ -4,8 +4,9 @@ using System.Text;
 
 namespace Prisma.Domain.Entities;
 
-public class Audit
+public class BaseEntity
 {
+    public required string Id { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt{ get; set; }
     public DateTime? DeletedAt{ get; set; }

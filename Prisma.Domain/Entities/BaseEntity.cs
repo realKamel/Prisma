131d1f52fbe630ce501de886d68a1a8aea7ad0b1
moaz-37 <moaz.sameh.37@gmail.com>
2026-06-06 01:12:54 +0300

    changed id type

--- Prisma.Domain/Entities/BaseEntity.cs
+++ Prisma.Domain/Entities/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Prisma.Domain.Entities;
 
 public class BaseEntity
 {
-    public required int Id { get; set; }
+    public int Id { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? UpdatedAt{ get; set; }
     public DateTime? DeletedAt{ get; set; }

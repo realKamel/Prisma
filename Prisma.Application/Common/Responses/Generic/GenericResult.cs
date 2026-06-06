5ded198f7eeb3b1f61a775990a82d83173a6705f
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-06 20:27:11 +0300

    Generic response

---
+++ Prisma.Application/Common/Responses/Generic/GenericResult.cs
@@ -0,0 +1,15 @@
+namespace Prisma.Application.Common.Responses.Generic;
+
+public class Result<T> : Result
+{
+    public T? Data { get; init; }
+    public object? Meta { get; init; }
+
+    private Result() { }
+
+    public static Result<T> Success(T data, string message = "Success", object? meta = null) =>
+        new() { Succeeded = true, Data = data, Message = message, Meta = meta };
+
+    public static new Result<T> Failure(string message, List<string>? errors = null) =>
+        new() { Succeeded = false, Message = message, Errors = errors ?? [] };
+}
--- Prisma.Application/Common/Responses/Result.cs
+++ Prisma.Application/Common/Responses/Result.cs
@@ -1,18 +1,16 @@
 namespace Prisma.Application.Common.Responses;
 
-public class Result<T>
+public class Result
 {
     public bool Succeeded { get; init; }
-    public string Message { get; init; }
-    public T Data { get; init; }
+    public string Message { get; init; } = string.Empty;
     public List<string>? Errors { get; init; }
-    public object? Meta { get; init; }
 
-    private Result() { }
+    protected Result() { }
 
-    public static Result<T> Success(T data, string message = "Success", object meta = null) =>
-        new() { Succeeded = true, Data = data, Message = message, Meta = meta };
+    public static Result Success(string message = "Success") =>
+        new() { Succeeded = true, Message = message };
 
-    public static Result<T> Failure(string message, List<string> errors = null) =>
+    public static Result Failure(string message, List<string>? errors = null) =>
         new() { Succeeded = false, Message = message, Errors = errors ?? [] };
 }

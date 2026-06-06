3ced531bb0298d5cfbc10d1700c8f0fbc326ff9a
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-06 03:51:59 +0300

    fixes

--- Prisma.Infrastructure/Persistence/AppDbContext.cs
+++ Prisma.Infrastructure/Persistence/AppDbContext.cs
@@ -5,14 +5,14 @@ using Prisma.Domain.Entities;
 
 namespace Prisma.Infrastructure.Persistence;
 
-
-public class AppDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser, IdentityRole, string>(options)
+public class AppDbContext(DbContextOptions options)
+    : IdentityDbContext<User, IdentityRole<Guid>, Guid>(options)
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         // to apply all configuration for class that implements IEntityTypeConfiguration
         base.OnModelCreating(modelBuilder);
-        
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }

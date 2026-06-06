2f95134cccbeb41bebc5fae6bee2f2a1b9110c80
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-06 03:16:05 +0300

    enhanced Generic Repository.cs implementation

--- Prisma.Infrastructure/Persistence/Repositories/Repository.cs
+++ Prisma.Infrastructure/Persistence/Repositories/Repository.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Prisma.Domain.Common;
+using Prisma.Domain.Entities;
 using Prisma.Domain.Interfaces;
 
 namespace Prisma.Infrastructure.Persistence.Repositories;
@@ -8,65 +8,73 @@ public class Repository<TEntity>(AppDbContext dbContext) :
     IRepository<TEntity> where TEntity : BaseEntity
 {
     public async Task<ICollection<TEntity>> ListAsync(ISpecification<TEntity> spec,
-        CancellationToken ct = default)
+        CancellationToken ct)
     {
         var query = SpecificationEvaluator
             .GetInstance
             .GetQuery(dbContext.Set<TEntity>(), spec);
 
-        return await query.ToListAsync(ct);
+        return await query.AsNoTracking().ToListAsync(ct);
     }
 
-    public async Task<ICollection<TEntity>> ListAsync(CancellationToken ct = default)
+    public async Task<ICollection<TEntity>> ListAsync(CancellationToken ct)
     {
-        return await dbContext.Set<TEntity>().ToListAsync(ct);
+        return await dbContext.Set<TEntity>().AsNoTracking().ToListAsync(ct);
     }
 
-    public async Task<TEntity?> GetBySpecAsync(ISpecification<TEntity> spec, CancellationToken ct = default)
+    public async Task<TEntity?> GetBySpecAsync(ISpecification<TEntity> spec, CancellationToken ct,
+        bool tracking = false)
     {
         var query = SpecificationEvaluator
             .GetInstance
             .GetQuery(dbContext.Set<TEntity>(), spec);
+
+        if (tracking)
+        {
+            query = query.AsNoTracking();
+        }
+
         return await query.FirstOrDefaultAsync(ct);
     }
 
-    public async Task<int> CountAsync(ISpecification<TEntity> spec, CancellationToken ct = default)
+    public async Task<int> CountAsync(ISpecification<TEntity> spec, CancellationToken ct)
     {
         var query = SpecificationEvaluator
             .GetInstance
             .GetQuery(dbContext.Set<TEntity>(), spec);
-        return await query.CountAsync(ct);
+
+        return await query.AsNoTracking().CountAsync(ct);
     }
 
-    public async Task<int> CountAsync(CancellationToken ct = default)
+    public async Task<int> CountAsync(CancellationToken ct)
     {
-        return await dbContext.Set<TEntity>().CountAsync(ct);
+        return await dbContext.Set<TEntity>().AsNoTracking().CountAsync(ct);
     }
 
-    public async Task<bool> AnyAsync(ISpecification<TEntity> spec, CancellationToken ct = default)
+    public async Task<bool> AnyAsync(ISpecification<TEntity> spec, CancellationToken ct)
     {
         var query = SpecificationEvaluator
             .GetInstance
             .GetQuery(dbContext.Set<TEntity>(), spec);
-        return await query.AnyAsync(ct);
+        return await query.AsNoTracking().AnyAsync(ct);
     }
 
-    public async Task<bool> AnyAsync(CancellationToken ct = default)
+    public async Task<bool> AnyAsync(CancellationToken ct)
     {
-        return await dbContext.Set<TEntity>().AnyAsync(ct);
+        return await dbContext.Set<TEntity>().AsNoTracking().AnyAsync(ct);
     }
 
-    public async Task AddAsync(TEntity entity, CancellationToken ct = default)
+    public async Task AddAsync(TEntity entity, CancellationToken ct)
     {
         await dbContext.Set<TEntity>().AddAsync(entity, ct);
     }
 
-    public void Update(TEntity entity, CancellationToken ct = default)
+    public void Update(TEntity entity)
     {
         dbContext.Set<TEntity>().Update(entity);
     }
 
-    public void Delete(TEntity entity, CancellationToken ct = default)
+    public void Delete(TEntity entity)
     {
         dbContext.Set<TEntity>().Remove(entity);
     }

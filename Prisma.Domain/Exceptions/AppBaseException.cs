1aae5715f59152e7b64b2d79738a66c1b50da146
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-10 21:04:35 +0300

    GlobalExceptionHandlingMiddleware.cs fixes

--- Prisma.Domain/Exceptions/AppBaseException.cs
+++ Prisma.Domain/Exceptions/AppBaseException.cs
@@ -1,5 +1,6 @@
 namespace Prisma.Domain.Exceptions;
 
-public abstract class AppBaseException(string message, string ErrorCode) : Exception(message)
+public abstract class AppBaseException(string message, string errorCode) : Exception(message)
 {
+    public string ErrorCode { get; init } = errorCode;
 }

ce4bbc343784aad4d8b963feaf8336fa041e6834
moaz-37 <moaz.sameh.37@gmail.com>
2026-06-15 21:32:20 +0300

    Add LessonMaterial

---
+++ Prisma.Domain/Entities/LessonAggregate/LessonMaterial.cs
@@ -0,0 +1,12 @@
+﻿using Prisma.Domain.Common;
+using Prisma.Domain.Enums;
+
+namespace Prisma.Domain.Entities.LessonAggregate;
+
+public class LessonMaterial: BaseEntity
+{
+    public string Title { get; set; } = string.Empty;
+    public string Size { get; set; } = string.Empty;
+    public LessonMaterialType Type { get; set; } 
+    public string DownloadUrl { get; set; } = string.Empty;
+}
---
+++ Prisma.Domain/Enums/LessonMaterialType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prisma.Domain.Enums;
+
+public enum LessonMaterialType
+{
+    PDF = 1, PPT
+}

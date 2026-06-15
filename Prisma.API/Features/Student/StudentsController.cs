fa89d86dfe7151c727a3c375bd7585a020b1721d
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-06-16 00:52:53 +0300

    Renaming StudentLessonsCatalogController.cs to StudentsController.cs

---
+++ Prisma.API/Features/Student/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Prisma.API.Common;
+using Prisma.Application.Common.Constants;
+using Prisma.Application.Common.Responses.Generic;
+using Prisma.Application.Features.LessonCatalog.Queries;
+
+namespace Prisma.API.Features.Student;
+
+public class StudentsController(ISender mediator) : ApiController
+{
+    [Authorize(Roles = AppClaims.Roles.Student)]
+    [ProducesResponseType<Result<ICollection<LessonCatalogDto>>>(StatusCodes.Status200OK)]
+    [HttpGet("catalog")]
+    public async Task<ActionResult> GetLessonsCatalog(CancellationToken c)
+    {
+        var result = await mediator.Send(new GetLessonsCatalogQuery(), c);
+        return Ok(result);
+    }
+}

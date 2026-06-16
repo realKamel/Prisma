c2af2f623cd2c8bc68fbc81b878165d78515736c
omnia shaaban <omniashaaban74@gmail.com>
2026-06-17 01:48:40 +0300

    Add expired lesson details endpoint and related logic

---
+++ Prisma.Application/Features/Lessons/Queries/GetLessonExpired/GetLessonExpiredQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using Prisma.Application.Common.Responses.Generic;
+
+namespace Prisma.Application.Features.Lessons.Queries.GetLessonExpired;
+
+public record GetLessonExpiredQuery(int LessonId) : IRequest<Result<LessonExpiredDto>>;
+public class LessonExpiredDto
+{
+
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public string Description { get; set; }= string.Empty;
+
+    public int ChaptersCount { get; set; }
+    public decimal Price { get; set; }
+
+    public int MaterialsCount { get; set; }
+
+    public double totalprogress { get; set; }
+
+    public decimal Degree { get; set; }
+
+
+    public DateTimeOffset? ExpiredDate { get; set; }
+    public int ValidityDays { get; set; }
+    public List<ChapterDto> Chapters { get; set; } = [];
+}
+
+public record ChapterDto(int Id, string Title, string Duration);
+
---
+++ Prisma.Domain/Specifications/Lessons/LessonExpiredSpecification.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ardalis.Specification;
+using Prisma.Domain.Entities.LessonAggregate;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Prisma.Domain.Specifications.Lessons;
+
+public class LessonExpiredSpecification : Specification<Lesson>
+{
+   public LessonExpiredSpecification(int lessonId)
+        : base()
+    {
+        Query.Where(lesson => lesson.Id == lessonId)
+             .Include(l => l.Enrollments)
+             .Include(l => l.Sections)
+             .ThenInclude(s => s.Progresses)
+             .Include(l=>l.LessonMaterials)
+             .Include(l => l.Quiz)
+             .ThenInclude(q => q.Attempts)
+             .AsNoTracking();
+
+    }
+
+}

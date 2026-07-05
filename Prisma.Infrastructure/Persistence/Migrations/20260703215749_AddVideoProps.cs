96b99ecf37911e4340044579af8c4a13a8f43ae8
moaz-37 <moaz.sameh.37@gmail.com>
2026-07-05 18:46:01 +0300

    Add video properties to Section

---
+++ Prisma.Infrastructure/Persistence/Migrations/20260703215749_AddVideoProps.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prisma.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVideoProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssetId",
+                table: "Section",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaybackId",
+                table: "Section",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UploadId",
+                table: "Section",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssetId",
+                table: "Section");
+
+            migrationBuilder.DropColumn(
+                name: "PlaybackId",
+                table: "Section");
+
+            migrationBuilder.DropColumn(
+                name: "UploadId",
+                table: "Section");
+        }
+    }
+}

7d1cc98989dcc10f8f967a7603206055c07c72e5
moaz-37 <moaz.sameh.37@gmail.com>
2026-07-12 04:11:01 +0300

    Fix email verification

--- Prisma.Application/Features/Authentication/Commands/EmailVerification/EmailVerificationHandler.cs
+++ Prisma.Application/Features/Authentication/Commands/EmailVerification/EmailVerificationHandler.cs
@@ -18,8 +18,7 @@ namespace Prisma.Application.Features.Authentication.Commands.EmailVerification;
 
 public class EmailVerificationRequestHandler(
     UserManager<User> _userManager,
-    IEmailService _emailService,
-    IConfiguration _config)
+    IEmailService _emailService)
     : IRequestHandler<EmailVerificationRequestCommand, Result>
 {
     public async Task<Result> Handle(EmailVerificationRequestCommand request, CancellationToken cancellationToken)
@@ -32,7 +31,7 @@ public class EmailVerificationRequestHandler(
         }
         var emailToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
-        var verificationLink = $"{_config["AppSettings:BaseUrl"]}/api/v1/auth/confirm-email?email={request.Email}&token={Uri.EscapeDataString(emailToken)}";
+        var verificationLink = $"http//localhost:5117/api/v1/auth/confirm-email?email={request.Email}&token={Uri.EscapeDataString(emailToken)}";
         await _emailService.SendAsync(request.Email, "Email Verification", $"Please verify your email by clicking on the following link: {verificationLink}");
         return Result.Success("Verification email sent successfully.");
     }

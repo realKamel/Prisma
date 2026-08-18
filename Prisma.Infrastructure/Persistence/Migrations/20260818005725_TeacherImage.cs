c1702b20a79ba2c7e0a38450c0e4c221c901e48b
moaz-37 <moaz.sameh.37@gmail.com>
2026-08-18 04:26:15 +0300

    added teacherimage

---
+++ Prisma.Infrastructure/Persistence/Migrations/20260818005725_TeacherImage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prisma.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeacherImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Backfill existing NULLs with distinct values before enforcing NOT NULL
+            migrationBuilder.Sql(@"
+                UPDATE ""Lesson""
+                SET ""PublicId"" = gen_random_uuid()
+                WHERE ""PublicId"" IS NULL;
+            ");
+
+                    migrationBuilder.Sql(@"
+                UPDATE ""AcademicYear""
+                SET ""PublicId"" = gen_random_uuid()
+                WHERE ""PublicId"" IS NULL;
+            ");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PublicId",
+                table: "Lesson",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PublicId",
+                table: "AcademicYear",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PublicId",
+                table: "Lesson",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PublicId",
+                table: "AcademicYear",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}

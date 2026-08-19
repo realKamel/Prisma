82e97107b1d4a7f376ec5f1ab46c2db917cd1513
abdelrahman <abdelrahman.kamel.dev@gmail.com>
2026-08-19 20:49:12 +0300

    Rename TeacherPreferencesController to PlatformConfigurationsController

--- Prisma.API/Features/Admins/PlatformConfigurationsController.cs
+++ Prisma.API/Features/Admins/PlatformConfigurationsController.cs
@@ -6,16 +6,16 @@ using Prisma.API.Common;
 using Prisma.Application.Common.Constants;
 using Prisma.Application.Features.TeacherPreferences.Commands.UpdateAccentColor;
 
+namespace Prisma.API.Features.Admins;
 
-namespace Prisma.API.Features.Teacher;
-
-[Authorize(Roles = AppRoles.Teacher)]
-[Route("api/v1/teacher/preferences")]
-public class TeacherPreferencesController(ISender sender) : ApiController
+[Authorize(Roles = AppRoles.Admin)]
+public class PlatformConfigurationsController(ISender sender) : ApiController
 {
     [HttpPut("accent")]
     public async Task<Result> UpdateAccentColor(
-        [FromBody] UpdateAccentColorCommand command, CancellationToken ct)
+        [FromBody] UpdateAccentColorCommand command,
+        CancellationToken ct
+    )
     {
         var result = await sender.Send(command, ct);
         return result;

92b463d1bba2bc5d5a9e82905a54d542a818fa82
moaz-37 <moaz.sameh.37@gmail.com>
2026-08-23 18:52:47 +0300

    added missing spec

---
+++ Prisma.Domain/Specifications/Users/UserWithProjectionSpec.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+using Ardalis.Specification;
+using Prisma.Domain.Entities.UserAggregate;
+
+namespace Prisma.Domain.Specifications.Users;
+
+public class UserWithProjectionSpec<TResult> : Specification<User, TResult>
+{
+    public UserWithProjectionSpec(Guid id, Expression<Func<User, TResult>> projection)
+    {
+        Query
+            .Where(u => u.Id == id)
+            .AsNoTracking()
+            .Select(projection);
+    }
+}

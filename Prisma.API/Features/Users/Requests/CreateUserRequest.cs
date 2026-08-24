5cc6f8dbb2d8b527bf9e838f69cbb90e6e5118a9
omnia shaaban <omniashaaban74@gmail.com>
2026-08-24 22:43:14 +0300

    Add optional Subject property to CreateUserRequest

--- Prisma.API/Features/Users/Requests/CreateUserRequest.cs
+++ Prisma.API/Features/Users/Requests/CreateUserRequest.cs
@@ -3,4 +3,4 @@ namespace Prisma.API.Features.Users.Requests;
 public record CreateUserRequest(
     string FirstName, string SecondName, string ThirdName, string LastName,
     string Mobile, string Email, string Password, string Role,
-    int? GradeId, Guid? TeacherId, string? ParentMobile);
+    int? GradeId, Guid? TeacherId, string? ParentMobile, string? Subject);

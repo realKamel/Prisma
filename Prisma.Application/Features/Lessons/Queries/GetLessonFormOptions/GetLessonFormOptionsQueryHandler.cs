b3feabd32d9eed9a0e2bc16141b85d32da04a8ba
omnia shaaban <omniashaaban74@gmail.com>
2026-08-25 23:21:54 +0300

    Enhance lesson form options with user-based filtering

--- Prisma.Application/Features/Lessons/Queries/GetLessonFormOptions/GetLessonFormOptionsQueryHandler.cs
+++ Prisma.Application/Features/Lessons/Queries/GetLessonFormOptions/GetLessonFormOptionsQueryHandler.cs
@@ -1,27 +1,44 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using MediatR;
 using Ardalis.Result;
+using MediatR;
+using Prisma.Application.Abstractions.Services;
 using Prisma.Application.Features.Lessons.Queries.GetLessonEditorDetails;
 using Prisma.Domain.Entities.LessonAggregate;
+using Prisma.Domain.Entities.UserAggregate;
 using Prisma.Domain.Interfaces;
+using Prisma.Domain.Specifications.Lessons;
 
 namespace Prisma.Application.Features.Lessons.Queries.GetLessonFormOptions;
 
 public class GetLessonFormOptionsQueryHandler(
-    IUnitOfWork unitOfWork
+    IUnitOfWork unitOfWork, IIdentityService identityService, ICurrentUserService currentUserService
 ) : IRequestHandler<GetLessonFormOptionsQuery, Result<LessonFormOptionsResponseDto>>
 {
     public async Task<Result<LessonFormOptionsResponseDto>> Handle(
         GetLessonFormOptionsQuery request,
         CancellationToken cancellationToken)
     {
+        var userId = currentUserService.UserId;
+        if (userId is null)
+            return Result.Unauthorized("User is not authenticated.");
+
+        var user = await identityService.FindByIdAsync(userId.Value, cancellationToken);
+        if (user is null)
+            return Result.NotFound("User not found.");
+
+        if (user is Assistant assistant)
+        {
+            if (assistant.TeacherId is null)
+                return Result.Unauthorized("Assistant is not associated with a teacher.");
+            userId = assistant.TeacherId;
+        }
         var lessonRepository = unitOfWork.GetOrCreateRepository<Lesson,int>();
-        var allLessons = await lessonRepository.ListAsync(cancellationToken);
-        var prerequisitesOptions = allLessons
-            .Select(l => new LessonDto(l.Title ?? string.Empty, l.Id))
-            .ToList();
+        var allLessons = await lessonRepository.ListAsync(
+            new LessonWithProjectionSpec<LessonDto>(userId.Value,
+            l => new LessonDto(l.Title ?? string.Empty, l.Id)),cancellationToken);
+        
 
         var academicYearRepository = unitOfWork.GetOrCreateRepository<AcademicYear, int>();
         var allAcademicYears = await academicYearRepository.ListAsync(cancellationToken);
@@ -30,7 +47,10 @@ public class GetLessonFormOptionsQueryHandler(
             .ToList();
 
         return Result<LessonFormOptionsResponseDto>.Success(
-            new LessonFormOptionsResponseDto(prerequisitesOptions, allAcademicYearsOptions)
+            new LessonFormOptionsResponseDto(allLessons, allAcademicYearsOptions)
         );
+
+
     }
 }
+
